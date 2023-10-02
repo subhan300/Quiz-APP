221a4c33991bb2449d22457b49dcc0e79b5a1667
unknown <subhan.akram2400@gmail.com>
2023-10-03 01:37:07 +0500

    meta data removed

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -9,36 +9,7 @@ import { SettingsDrawer, SettingsProvider } from "src/components/settings";
 
 // ----------------------------------------------------------------------
 
-export const metadata = {
-  title: "EFSET",
-  description: "EFSET QUIZ Application",
-  keywords: "react,material,kit,application,dashboard,admin,template",
-  themeColor: "#000000",
-  manifest: "/manifest.json",
-  icons: [
-    {
-      rel: "icon",
-      url: "/favicon/favicon.ico",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "16x16",
-      url: "/favicon/favicon-16x16.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      sizes: "32x32",
-      url: "/favicon/favicon-32x32.png",
-    },
-    {
-      rel: "apple-touch-icon",
-      sizes: "180x180",
-      url: "/favicon/apple-touch-icon.png",
-    },
-  ],
-};
+
 
 export default function RootLayout({ children }) {
   return (
